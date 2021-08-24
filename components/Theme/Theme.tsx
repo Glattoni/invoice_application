e1efdd5958c73896c392de3df305d9eae3b90ea0
Alexey Parastyuk <glattoni8@gmail.com>
2021-08-24 12:47:46 +0300

    refactor theme toggling functionalitty

--- components/Theme/Theme.tsx
+++ components/Theme/Theme.tsx
@@ -1,11 +1,10 @@
 import React, { FC, ReactNode } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { useThemeContext } from '../../contexts/global_theme/ThemeStore';
+import { useThemeContext } from '../../contexts/themeContext/ThemeStore';
 import { themes } from './themes';
 
 const Theme: FC<ReactNode> = ({ children }) => {
   const { theme } = useThemeContext();
-
   return (
     <ThemeProvider theme={theme && themes[theme]}>{children}</ThemeProvider>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeStore } from '../contexts/global_theme/ThemeStore';
+import { ThemeStore } from '../contexts/themeContext/ThemeStore';
 import { Theme } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
